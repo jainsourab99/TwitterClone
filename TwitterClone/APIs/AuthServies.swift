c2d23faa5a2e9f3f3f6240ba524d724dd9b2c5f5
Sourabh Jain <42956699+jainsourab99@users.noreply.github.com>
2023-12-27 09:38:56 +0530

    Initial Commit
    Twitter Demo App in Swift with programmatic UI

--- TwitterClone/APIs/AuthServies.swift
+++ TwitterClone/APIs/AuthServies.swift
@@ -24,38 +24,40 @@ class AuthServies {
     func logUserIn(withEmail: String, password: String, completion: @escaping(AuthDataResult?, Error?) -> Void) {
         Auth.auth().signIn(withEmail: withEmail, password: password, completion: completion)
     }
-    func registerUser(credentials: AuthCredentials, completion: @escaping(Error?, DatabaseReference) -> Void) {
-        
-        let email = credentials.email
-        let password = credentials.password
-        let userName = credentials.userName
-        let fullName = credentials.fullName
-
-
-        guard let imageData = credentials.profileImage.jpegData(compressionQuality: 0.3) else{return}
+    func registerUser(credential: AuthCredential, completion: @escaping(Error?, DatabaseReference) -> Void) {
+        let email = credential.email
+        let password = credential.password
+        let userName = credential.userName
+        let fullName = credential.fullName
+        guard let imageData = credential.profileImage.jpegData(compressionQuality: 0.3) else { return }
         let fileName = NSUUID().uuidString
-        let storageRef = STORAGE_PROFILE_IMAGES.child(fileName)
-        storageRef.putData(imageData, metadata: nil) { Result, error in
-            storageRef.downloadURL { url, error in
-                guard let profileImageURL = url?.absoluteString else {return}
-                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        
+        let storageRef = STOGARE_PROFILE_REF.child(fileName)
+        storageRef.putData(imageData, metadata: nil) { (meta, error) in
+            storageRef.downloadURL { (url, error) in
+                guard let profileImageURL = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
                     if let error = error {
-                        debugPrint("DEBUG: ERROR is \(error.localizedDescription)")
+                        print(error.localizedDescription)
                         return
                     }
-                    
-                    guard let uid = result?.user.uid else{return}
+                    guard let uid = result?.user.uid else { return }
                     
                     let values = ["email": email,
                                   "userName": userName,
                                   "fullName": fullName,
                                   "profileImageURL": profileImageURL]
                     
-                    REF_USERS.child(uid).updateChildValues(values, withCompletionBlock: completion)
+                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
+                        print("updateChildValues")
+                        completion(error, ref)
+                    }
                 }
             }
         }
     }
+
 }
 
 
